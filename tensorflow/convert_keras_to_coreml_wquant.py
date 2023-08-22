09eefdae350adef4a2845beb6dbe38fa712ed60c
giild <peter@giild.com>
2023-08-22 10:21:41 -0400

    making the script more generalized

--- tensorflow/convert_keras_to_coreml_wquant.py
+++ tensorflow/convert_keras_to_coreml_wquant.py
@@ -18,20 +18,25 @@ def main():
     else:
         input = args[1].replace("\\","/")
         output_file = args[2].replace("\\","/")
+        author = args[3]
+        license = args[4]
+        mversion = args[5]
+        sdesc = args[6]
         output_file = output_file + ".mlpackage"
         print(' converting H5 models to Apple coreml format ', input)
         output_labels = ["rock","paper","scissor"]
         h5model = tf.keras.models.load_model(input)
+        # the shape needs to be changed to match your model
         image_input = coremltools.ImageType(name="conv2d_input", shape=(1,300,300,3))
         classifier_config = coremltools.ClassifierConfig(output_labels)
         ctmodel = coremltools.convert(h5model,
                                       inputs=[image_input],
                                       classifier_config=classifier_config,
                                       source='tensorflow')
-        ctmodel.author = "Peter Lin / woolfel@gmail.com"
-        ctmodel.version = "0.8"
-        ctmodel.license = "apache"
-        ctmodel.short_description = "experimental model for rockpaperscissor trained on apple silicon"
+        ctmodel.author = author
+        ctmodel.version = mversion
+        ctmodel.license = license
+        ctmodel.short_description = sdesc
         ctmodel = coremltools.models.neural_network.quantization_utils.quantize_weights(ctmodel,8)
         ctmodel.save(output_file)
 
