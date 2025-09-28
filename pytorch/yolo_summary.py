97173c60c099c9c70f889e1c1b3b2ea784c61809
giild <peter@giild.com>
2025-09-27 20:48:21 -0400

    yolov12 format is different, have to get ema

---
+++ pytorch/yolo_summary.py
@@ -0,0 +1,38 @@
+import argparse
+import sys
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torchsummary import summary
+from ultralytics import YOLO
+
+def main():
+    """
+    Main entry point for the script.
+    """
+    parser = argparse.ArgumentParser(
+        description="Try to use torchsummary to print a pytorch model. If it fails print the ordered keys."
+    )
+    parser.add_argument(
+        "filepath", 
+        type=str, 
+        help="Path to the filename"
+    )
+    args = parser.parse_args()
+    print("PyTorch Version:", torch.__version__)
+    model = torch.load(args.filepath, map_location=torch.device('cpu'), weights_only=False)
+    printSummary(model, args.filepath)
+
+def printSummary(model, filename):
+    print(f"File: {filename}")
+    model = model['ema']
+    try:
+        summary(model)
+    except Exception as e:
+        print(f"Summary: {model.keys()}")
+
+if __name__ == "__main__":
+    main()
